12086f2873e88c0536f8faef37811e1a3ec9d328
fatoubintou311 <fatoubinetousow55@gmail.com>
2023-10-02 22:08:17 +0200

    My first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args ){
+        System.out.println("hello");
+
+        Fraction f =new Fraction();
+        System.out.println(f);
+    }
+    
+}
